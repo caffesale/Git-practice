23f983dd96ab6e98a4b919d5081ff39dd8c34cad
pglibrary80 <pglibrary80@gmail.com>
2022-07-31 22:17:53 +0900

    ADD styled-components
    complete assignment ver1.0

--- src/pages/TodoDetail.jsx
+++ src/pages/TodoDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation, useNavigate }from 'react-router-dom'
-
+import styled from "styled-components";
 
 function DetailPage() {
     const location = useLocation();
@@ -9,20 +9,73 @@ function DetailPage() {
     
 
     return (
-        <div>
-            <div>
-                <div>{todo.id}</div>
-                <button onClick={() => {
-                    console.log(navi)
-                    navi(-1)
-                }}>prev</button>
-            </div>
-            <div>
-                <h2>{todo.title}</h2>
-                <div>{todo.body}</div>
-            </div>
-        </div>
+        <Container>
+            <Card>
+                <CardContent>
+                    <CardTitle>{todo.title}</CardTitle>
+                    <p className="card-id">{todo.id}</p>
+                    <p className="card-body">{todo.body}</p>
+                    <Btn onClick={() => {
+                        console.log(navi)
+                        navi(-1)
+                    }}>이전으로</Btn>
+                </CardContent>
+            </Card>
+        </Container>
     )
 }
 
 export default DetailPage;
+
+const Container = styled.div`
+    display: grid;
+    min-height: 100vh;
+    place-items: center;
+    line-height: 1.6;
+    font-family: sans-serif;
+`;
+
+const Card = styled.div`
+    color: #eee;
+    background-color: tomato;
+    background-size: cover;
+    padding: 10rem 0 0;
+    max-width: 35ch;
+    overflow: hidden;
+`
+const CardContent = styled.div`
+    padding: 1.5em;
+    background: linear-gradient(
+        hsl(0 0% 0%/ 0),
+        hsl(20 0% 0%/ 0.3) 20%,
+        hsl(0 0% 0%/ 0.5)
+    );
+`
+
+const CardTitle = styled.h2`
+    position: relative;
+    width: max-content;
+
+    &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        height: 4px;
+        width: 100%;
+        background-color: olive;
+    }
+`
+
+const Btn = styled.button`
+    cursor: pointer;
+    display: inline;
+    background-color: olive;
+    text-decoration: none;
+    padding: 0.5rem 1.25rem;
+
+    &:hover,
+    &:focus {
+        background-color: yellowgreen;
+    }
+`
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-
 import Layout from "../components/layout/Layout";
 import Header from "../components/header/Header";
 import Form from "../components/form/Form";
